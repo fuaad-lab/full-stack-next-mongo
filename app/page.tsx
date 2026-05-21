99284887c17f2b72cf0e4a7099c2b343e830a51f
fuaad ibraahim <fuaadi174@gamil.com>
2026-05-21 17:51:21 +0300

    waan xaliyay error kii

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import TodoForm from "./_components/todo/TodoForm";
 import Todos from "./_components/todo/Todos";
 
+export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <main className="w-full flex items-center justify-center flex-col max-w-lg mx-auto mt-32">
