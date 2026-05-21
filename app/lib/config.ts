424b34c51af75177a0d2c058fbc056320ae9220a
fuaad ibraahim <fuaadi174@gamil.com>
2026-05-21 18:00:51 +0300

    waan xaliyay error kii hore

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV=== 'production' ? '' : "http://localhost:3000";
+export const API_URL = process.env.NODE_ENV=== 'production' ? 'https://full-stack-next-mongo-ahsz.vercel.app/' : "http://localhost:3000";
